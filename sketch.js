d2e8609559283cc4e65fb78aa64ad3e88c32171d
SallyTheDandy <168108048+SallyTheDandy@users.noreply.github.com>
2024-04-25 13:43:40 -0500

    added replication on click and rotation

--- sketch.js
+++ sketch.js
@@ -14,6 +14,7 @@ let righteyes = []
 let lefteyes = []
 let currentrighteyeno 
 let currentrighteye
+//add number variable to resist shuffle
 let currentlefteyeno 
 let currentlefteye
 let currentmouth
@@ -21,10 +22,16 @@ let currentmouthno
 let prevYrot
 let prevXrot
 let varble
-let garble
+let garble, hugh,satration,brightnes
+let prevsatration
+let prevhue
 let bgHue =0
 let bgSaturation =0
 let bgBrightness =86
+let friend
+let friends = []
+let friendsXcoordinates = []
+let friendsYcoordinates = []
 function setup() {
   colorMode(HSB)
   createCanvas(windowWidth, windowHeight,WEBGL);
@@ -32,10 +39,14 @@ function setup() {
   prevYrot=0
   prevXrot=0
   garble=0
+  prevhugh=2
+  prevsatration=0
   varble=0
   currentmouthno=1
   currentlefteyeno=1
   currentrighteyeno=1
+
+  friend=new Friend(100,100)
  // frameRate(24)
   //sans= loadFont('COMICSANS.TTF')
   //currentrighteyeno=floor(random(0,eyetypes-1))
@@ -64,6 +75,8 @@ function preload(){
   //str(i)
 
 function draw() {
+  stroke(bgHue, bgSaturation, bgBrightness)
+  background(bgHue, bgSaturation, bgBrightness);
   //noStroke()
   let locX = mouseX - height / 2;
   let locY = mouseY - width / 2;
@@ -71,14 +84,14 @@ function draw() {
   ambientLight(255);
   //pointLight(100, 100, 100, locX, locY, 100);
  // pointLight(map(mouseX,0,width,0,255),map(mouseY,0,height,0,255),map(abs(pmouseX-mouseX),0,40,100,255), locX, locY, 100)
-  strokeWeight(2)
-  stroke(bgHue, bgSaturation, bgBrightness)
   currentlefteyeno = floor(map(mouseX,0,width,1,16,1))
   //fill(map(mouseX,0,width,0,255),map(mouseY,0,height,0,255),map(abs(pmouseX-mouseX),0,40,100,255))
-  let hugh=map(mouseX,0,width,0,255)
-  let satration =map(abs(pmouseX-mouseX),0,40,40,100)
+  hugh=lerp(prevhugh,map(mouseX,0,width,0,255),.9)
+   prevhugh=hugh
+   satration =lerp(prevsatration,map(abs(pmouseX-mouseX),0,40,40,100),.05)
+   prevsatration=satration
  // let brightnes=map(abs(pmouseX-mouseX),0,40,255,0)
- let brightnes=80
+  brightnes=80
   fill(hugh,satration,brightnes)
   currentrighteyeno = floor(map(mouseY,0,height,1,16,1))
   //figure out color
@@ -90,11 +103,14 @@ function draw() {
     lefteyes=shuffle(lefteyes)
     righteyes=shuffle(righteyes)
   }
+  if((abs(pmouseX-mouseX)>50)){
+    //hugh=20
+  }
   currentrighteye = righteyes[currentrighteyeno]
   currentlefteye = lefteyes[currentlefteyeno]
   currentmouth=mouths[currentmouthno]
 //strokeWeight(20)
-  background(bgHue, bgSaturation, bgBrightness);
+
   push()
    //normalMaterial();
    //shininess(50)
@@ -112,6 +128,8 @@ garble=lerp(prevXrot,-map(mouseY,0,height,-90,90)/2,1.9)
   //rotateY(frameCount * .05);
  //rotateY(-mouseY/width*200)
  box(100, 100, 110, cubeSize);
+ strokeWeight(2)
+ stroke(bgHue, bgSaturation, bgBrightness)
   roundedCube(cubeHeight,cuberadius)
   //rotateX(90)
   //rotateX(90)
@@ -123,6 +141,10 @@ garble=lerp(prevXrot,-map(mouseY,0,height,-90,90)/2,1.9)
   image(currentmouth,0,0,130,130)
 
   pop()
+  friends.forEach(f=>{
+    f.display()
+    f.orbit()
+  })
   prevYrot=varble
   prevXrot=garble
  // tint(236, 112, 255,100)
@@ -138,12 +160,20 @@ garble=lerp(prevXrot,-map(mouseY,0,height,-90,90)/2,1.9)
 function roundedCube(cubeWidth, radius) {
   push()
  // lights();
-//roundedcube code by flomerboy! their sketch is herehttps://editor.p5js.org/flomerboy/sketches/uq7nB_Dy
+//roundedcube code by flomerboy! their sketch is here https://editor.p5js.org/flomerboy/sketches/uq7nB_Dy
   //walls
+  //fill(map(mouseX,0,width,255,0),map(mouseY,0,height,0,100),brightnes)
+  //left and right
+  //fill(255-hugh,satration,brightnes-7)
   box(cubeWidth, cubeWidth - radius, cubeWidth - radius);
+  //specularMaterial(255);
+  //top and bottom
+  //fill(hugh-20,satration,brightnes-7)
   box(cubeWidth - radius, cubeWidth, cubeWidth - radius);
+  //front and back
+  //fill(hugh,satration,brightnes)
   box(cubeWidth - radius, cubeWidth - radius, cubeWidth);
-  fill(map(mouseX,0,width,0,255),map(mouseY,0,height,0,255),map(abs(pmouseX-mouseX),0,40,100,255))
+  fill(hugh,satration,map(abs(pmouseX-mouseX),0,40,100,255))
   //corners
   translate((cubeWidth - radius) / 2, (cubeWidth - radius) / 2, (cubeWidth - radius) / 2);
   sphere(radius / 2, 10, 10);
@@ -196,8 +226,60 @@ function roundedCube(cubeWidth, radius) {
 }
 function mouseClicked()
 {
-//bgHue=floor(random(0,255))
-//bgSaturation=floor(random(0,50))
+//friendsXcoordinates.push(mouseX)
+//friendsYcoordinates.push(mouseY)
+friends.push(new Friend(mouseX-width/2,mouseY-height/2,-200))
+bgHue=floor(random(0,255))
+bgSaturation=floor(random(0,10))
 //bgBrightness=floor(random(50,100))
 
+}
+class Friend {
+  constructor(x,y,z){
+    this.pos=createVector(x,y,z)
+    this.size=50
+    this.rotXspeed=random(-4,4)
+    this.rotYspeed=random(-4,4)
+    this.rotZspeed=random(-4,4)
+    this.scale=random(.3,.8)
+    this.myrighteye=currentrighteye
+    this.mylefteye= currentlefteye
+    this.mouthno=currentmouthno
+    this.orbitXspeed=random(-2,2)
+    this.orbitYspeed=random(-2,2)
+    this.orbitZspeed=random(-1,1)
+    this.hue=hugh
+    this.saturation=satration
+    this.brightness=brightnes
+  }
+  display(){
+    push()
+    translate(this.pos.x,this.pos.y,this.pos.z)
+    scale(this.scale)
+    rotateY(frameCount*this.rotYspeed)
+
+  rotateX(frameCount*this.rotXspeed)
+  rotateZ(frameCount*this.rotZspeed)
+    //box(100)
+    //noStroke()
+    //stroke('black')
+    strokeWeight(2)
+    fill(this.hue,this.saturation,this.brightness)
+    roundedCube(cubeHeight,cuberadius)
+    translate(15,-30,cubeWidth/2+10)
+  image(this.myrighteye,0,0,50,50)
+  translate(-80,0,0)
+  image(this.mylefteye,0,0,50,50)
+  translate(0,-35,1)
+  image(mouths[this.mouthno],0,0,130,130)
+   pop()
+  }
+  orbit(){
+  //rotateX(frameCount*this.orbitXspeed)
+  //rotateY(frameCount*this.orbitYspeed)
+ rotateZ(frameCount*this.orbitZspeed)
+
+  }
+    //rect(this.pos.x,this.pos.y,100,100);
+ // Friend.box(cubeWidth,cubeHeight)
 }
